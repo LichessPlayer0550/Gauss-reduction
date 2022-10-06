9104a5646b3700e9705081540fc7b129981c3edf
LichessPlayer0550 <97228779+LichessPlayer0550@users.noreply.github.com>
2022-10-06 13:47:58 +0200

    Update GaussReduction.py

--- GaussReduction.py
+++ GaussReduction.py
@@ -4,7 +4,7 @@ def GaussReduction(q, k=set()):
     ## computes a decomposition of the quadratic form associated to the matrix q in a sum of independant linear forms ##
     ## example of usage : let q(x,y) = x^2 + 2y^2 - 6xy in dimension 2, here the matrix of q is (1 -3)
     ##                                                                                          (-3 2)
-    ## hence we call GaussReduction([[1,-3],[-3,2]]), giving ((1.0, [1.0, -3.0]), (-7.0, [0, 1.0]))
+    ## hence we call GaussReduction([[1,-3],[-3,2]]), giving [(1.0, [1.0, -3.0]), (-7.0, [0, 1.0])]
     ## this means that q(x,y) = x^2 + 2y^2 - 6xy = (x-3y)^2 - 7y^2
     ##
     n = len(q)
@@ -17,14 +17,14 @@ def GaussReduction(q, k=set()):
                 for i in range(n+len(k)):
                     if i not in k:
                         if b==p or b==s: k.add(i)
-                        c.append((q[p][b]+q[s][b])/a)
-                        d.append((q[s][b]-q[p][b])/a)
+                        c.append((q[p][b]+q[b][s])/a)
+                        d.append((q[s][b]-q[b][p])/a)
                         b+=1
                     else:
                         c.append(0)
                         d.append(0)
                 c = [(a/(2*(int(p!=s)+1)),c)]
-                d = [None,[(-1,d)]][set(d)!={0}]
+                d = [None,[(-a/(2*(int(p!=s)+1)),d)]][set(d)!={0}]
                 yield c
                 if d: yield d
                 q = [[q[i][j]-(q[p][i]*q[s][j]+q[p][j]*q[s][i])/a for j in range(n)]for i in range(n)]
